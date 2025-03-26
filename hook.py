77c4b6580d8de525a9398b5b832b385323748d76
Kamil Krzyśków <kamilzary@gmail.com>
2025-03-26 23:15:48 +0100

    docs: pdf and hook fixup

--- hook.py
+++ hook.py
@@ -1,4 +1,18 @@
+import os
+from pathlib import Path
+
 from mkdocs.plugins import event_priority
+
+
+def on_config(config):
+
+    # Create file:// protocol root for PDF templates
+    # Using base "/assets" path resolves it based on the opened file
+    # This leads to net::ERR_FILE_NOT_FOUND so we have to define the root manually
+    # TODO For the plugin make a macros filter `file_url`
+    root = Path(config.site_dir).as_posix().rstrip("/")
+    config.extra["site_file_proto_root"] = f"file://{root}"
+
 
 @event_priority(100)
 def on_page_markdown(markdown, page, config, files):
